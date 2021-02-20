d5623b718faf242bc5e8f9847980cea6ac8f1f6e
Clayton C <clay578@gmail.com>
2021-02-19 19:10:46 -0800

    init

---
+++ src/server.cpp
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 4040
+
+int main(int argc, char const *argv[])
+{
+
+}//main
